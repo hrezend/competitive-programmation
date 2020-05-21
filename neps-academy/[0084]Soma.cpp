e58d384931a881fb3d96f4ccb1de4319da6e560e
Hérson Rezende <herson.academico@gmail.com>
2020-05-21 01:23:14 -0300

    Algorithm 21/05/2020

---
+++ neps-academy/[0084]Soma.cpp
@@ -0,0 +1,26 @@
+//Author: Herson Rezende - 2ºSM de CC - UFBA - 2020
+//Site: hrezend.github.io
+#include <bits/stdc++.h>
+#define desync ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pb push_back
+#define endl '\n'
+#define debug(x) cout << x << endl
+#define sz(x) (int)x.size()
+#define all(x) (x.begin(), x.end())
+#define rall(x) (x.rbegin(), x.rend())
+
+using namespace std;
+typedef long long ll;
+typedef pair <int, int> pii;
+typedef vector <int> vi;
+typedef vector <pii> vii;
+typedef vector <vi> matriz;
+
+int main(){
+	desync;
+
+	int X, Y;
+	cin >> X >> Y;
+
+	cout << X+Y << endl;
+}
