674fb9387949cbf80f024abb570d316f30be9c0e
Hérson Rezende <herson.academico@gmail.com>
2020-05-21 00:45:14 -0300

    Algorithm 21/05/2020

---
+++ code-forces/README.md
@@ -0,0 +1,5 @@
+## code-forces (http://codeforces.com/).
+
+Repositório com soluções feitas por mim em diversas linguagens para os problemas disponíveis na plataforma "Code Forces".
+
+As resoluções estão disponíveis em disversas linguagens nesta mesma pasta do repositório, portanto busque com atenção a do seu interesse.
---
+++ neps-academy/[0154]Tabuada.cpp
@@ -0,0 +1,28 @@
+//Author: Herson Rezende - 2ºSM de CC - UFBA - 2020
+//Site: hrezend.github.io
+#include <bits/stdc++.h>
+#define desync ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pb push_back
+#define endl '\n'
+#define debug(x) cout << x << endl
+#define sz(x) (int)x.size()
+#define all(x) (x.begin(), x.end())
+#define rall(x) (x.rbegin(), x.rend())
+
+using namespace std;
+typedef long long ll;
+typedef pair <int, int> pii;
+typedef vector <int> vi;
+typedef vector <pii> vii;
+typedef vector <vi> matriz;
+
+int main(){
+
+	int N;
+	cin >> N;
+
+	for(int i = 1; i <= 10; i++){
+		cout << N << " * " << i << " = " << N*i << endl;
+	}
+
+}
