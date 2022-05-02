3422ce508874dce7f075d45cd3a22a789cdc0079
Hérson Reis Rezende dos Santos <62779767+hrezend@users.noreply.github.com>
2022-05-02 09:13:18 -0300

    Create [Conditional]Pegar_Ou_Nao_Pegar.cpp

---
+++ jude-contest/[Conditional]Pegar_Ou_Nao_Pegar.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int a, b, c, d, e;
+	cin >> a >> b >> c >> d >> e;
+	
+	if((a+b+c+d+e)/5 % 2 == 0){
+  		cout << "Pegarrrrr." << endl;
+	}
+	else{
+  		cout<<"Do not take it, Cheburashka!!!"<<endl;
+	}
+}
